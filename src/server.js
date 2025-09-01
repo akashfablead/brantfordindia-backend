4d827b3fc88e6eb854557210513b224cdd7b691c
akash fablead <akash.fablead@gmail.com>
2025-09-01 10:54:54 +0530

    .env problem fix

--- src/server.js
+++ src/server.js
@@ -20,6 +20,6 @@ const PORT = process.env.PORT || 5000;
 //     console.log(`🚀 Server running at http://localhost:${PORT}  .`);
 // });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`✅ Server running on port ${PORT}`);
 });
