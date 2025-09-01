97f88d80061bcf4a400df083e64b07bcddee9445
akash fablead <akash.fablead@gmail.com>
2025-09-01 13:39:39 +0530

    Remove .env from git tracking

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
 
-const PORT = process.env.BACKEND_URL || 5000;
+const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
 //     console.log(`🚀 Server running at http://localhost:${PORT}  .`);
 // });
